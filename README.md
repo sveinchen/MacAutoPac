54a8706b340505063eae088ca1d64ef2078361ee
sveinchen <sveinchen@gmail.com>
2017-03-22 21:08:13 +0800

    update install.sh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MacAutoPac
 Update Automatic Proxy Configuration automatically according to Wi-Fi SSID for mac OS
+
+## Install
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/sveinchen/MacAutoPac/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -6,6 +6,17 @@ PLIST_URL="https://raw.githubusercontent.com/sveinchen/MacAutoPac/master/io.gith
 BIN_PATH="/usr/local/bin/autopac"
 PLIST_PATH="/Library/LaunchDaemons/${PLIST_URL##*/}"
 
+INI_PATH="/usr/local/etc/autopac.ini"
+read -r -d '' INI_TMPL <<- EOF
+[AutopacDefaults]
+;; pac_url = http://example.com/path/to/proxy.pac
+;; static_addr = 127.0.0.1
+
+[WiFI-SSID]
+;; pac_url = http://another.com/path/to/proxy.pac
+;; static_addr = 10.0.0.1
+EOF
+
 curl -fsSL $AUTOPAC_URL > $BIN_PATH
 
 chmod +x $BIN_PATH
@@ -19,4 +30,8 @@ curl -fsSL $PLIST_URL  | sudo tee $PLIST_PATH > /dev/null
 
 cd ${PLIST_PATH%/*} && sudo launchctl load -w ${PLIST_URL##*/}
 
+touch $INI_PATH
+echo "$INI_TMPL" > $INI_PATH
+
+echo ">>> Edit config file at: \"$INI_PATH\"" 
 echo ">>> enjoy!"
